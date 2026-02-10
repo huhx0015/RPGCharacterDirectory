70cc951f9d15f9076743938155fc0d077bec41a7
Michael Y. Huh <huhx0015@users.noreply.github.com>
2026-02-09 16:35:40 -0800

    Repurpose GoTimer as RPGCharacterDirectory, add JSON asset files, update all dependencies, update target SDK to 36.

--- app/src/main/java/com/huhx0015/gotimer/State.kt
+++
@@ -1,5 +0,0 @@
-package com.huhx0015.gotimer
-
-enum class State {
-    NOT_STARTED, RUNNING, PAUSED, FINISHED
-}
---
+++ app/src/main/java/com/huhx0015/rpgcharacterdirectory/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huhx0015.rpgcharacterdirectory
+
+import androidx.activity.ComponentActivity
+
+class MainActivity: ComponentActivity() {
+}
