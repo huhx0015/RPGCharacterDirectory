72438688be02d3bc7e40afc55ebcb27f0521a38a
Michael Y. Huh <huhx0015@users.noreply.github.com>
2026-02-24 16:32:41 -0800

    Load all character JSON files, updated UI

--- app/src/main/java/com/huhx0015/rpgcharacterdirectory/ui/CharacterListViewModel.kt
+++ app/src/main/java/com/huhx0015/rpgcharacterdirectory/ui/CharacterListViewModel.kt
@@ -14,6 +14,23 @@ class CharacterListViewModel: ViewModel() {
 
   companion object {
     private const val FILE_JSON_FINAL_FANTASY_II = "final_fantasy_ii.json"
+    private const val FILE_JSON_FINAL_FANTASY_IV = "final_fantasy_iv.json"
+    private const val FILE_JSON_FINAL_FANTASY_V = "final_fantasy_v.json"
+    private const val FILE_JSON_FINAL_FANTASY_VI = "final_fantasy_vi.json"
+    private const val FILE_JSON_FINAL_FANTASY_VII = "final_fantasy_vii.json"
+    private const val FILE_JSON_FINAL_FANTASY_VIII = "final_fantasy_viii.json"
+    private const val FILE_JSON_FINAL_FANTASY_IX = "final_fantasy_ix.json"
+    private const val FILE_JSON_FINAL_FANTASY_X = "final_fantasy_x.json"
+    private const val FILE_JSON_FINAL_FANTASY_XII = "final_fantasy_xii.json"
+    private const val FILE_JSON_FINAL_FANTASY_XIII = "final_fantasy_xiii.json"
+    private const val FILE_JSON_FINAL_FANTASY_XV = "final_fantasy_xv.json"
+    private const val FILE_JSON_FINAL_FANTASY_XVI = "final_fantasy_xvi.json"
+    private const val FILE_JSON_SUIKODEN = "suikoden.json"
+    private const val FILE_JSON_SUIKODEN_II = "suikoden_ii.json"
+    private const val FILE_JSON_SUIKODEN_III = "suikoden_iii.json"
+    private const val FILE_JSON_SUIKODEN_IV = "suikoden_iv.json"
+    private const val FILE_JSON_SUIKODEN_V = "suikoden_v.json"
+
     private const val FILE_JSON_LEADERS = "leaders.json"
     private val TAG = CharacterListViewModel::class.java.simpleName
   }
@@ -25,6 +42,103 @@ class CharacterListViewModel: ViewModel() {
       jsonFileName = FILE_JSON_FINAL_FANTASY_II
     ) ?: emptyList()
     characterListMap[FILE_JSON_FINAL_FANTASY_II] = ff2CharacterList
+
+    val ff4CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_IV
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_IV] = ff4CharacterList
+
+    val ff5CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_V
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_V] = ff5CharacterList
+
+    val ff6CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_VI
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_VI] = ff6CharacterList
+
+    val ff7CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_VII
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_VII] = ff7CharacterList
+
+    val ff8CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_VIII
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_VIII] = ff8CharacterList
+
+    val ff9CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_IX
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_IX] = ff9CharacterList
+
+    val ff10CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_X
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_X] = ff10CharacterList
+
+    val ff12CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_XII
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_XII] = ff12CharacterList
+
+    val ff13CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_XIII
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_XIII] = ff13CharacterList
+
+    val ff15CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_XV
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_XV] = ff15CharacterList
+
+    val ff16CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_FINAL_FANTASY_XVI
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_FINAL_FANTASY_XVI] = ff16CharacterList
+
+    val s1CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_SUIKODEN
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_SUIKODEN] = s1CharacterList
+
+    val s2CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_SUIKODEN_II
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_SUIKODEN_II] = s2CharacterList
+
+    val s3CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_SUIKODEN_III
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_SUIKODEN_III] = s3CharacterList
+
+    val s4CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_SUIKODEN_IV
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_SUIKODEN_IV] = s4CharacterList
+
+    val s5CharacterList = JsonUtil.getJsonCharacterDataFromAsset(
+      context = context,
+      jsonFileName = FILE_JSON_SUIKODEN_V
+    ) ?: emptyList()
+    characterListMap[FILE_JSON_SUIKODEN_V] = s5CharacterList
+
     Log.d(TAG, "Character data size: ${characterListMap.size}")
 
     val leaderMap: Map<Int, RPGCharacter>
--- app/src/main/java/com/huhx0015/rpgcharacterdirectory/ui/compose/CharacterComposeScreen.kt
+++ app/src/main/java/com/huhx0015/rpgcharacterdirectory/ui/compose/CharacterComposeScreen.kt
@@ -3,13 +3,16 @@ package com.huhx0015.rpgcharacterdirectory.ui.compose
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -32,19 +35,24 @@ fun CharacterComposeScreen(characterList: List<RPGCharacter>) {
     // Compose UI for each item in characterList.
     items(characterList) { rpgCharacter ->
       CharacterComposeRow(character = rpgCharacter)
+      Spacer(modifier = Modifier.height(4.dp))
     }
   }
 }
 
 @Composable
 private fun CharacterComposeRow(character: RPGCharacter) {
-  Row(
-    modifier = Modifier.fillMaxWidth()
-  ) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-      Text(character.name)
-      Text(character.characterClass)
-      Text(character.game)
+  Card(modifier = Modifier.fillMaxWidth()) {
+    Row(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(12.dp)
+    ) {
+      Column(modifier = Modifier.fillMaxWidth()) {
+        Text(character.name)
+        Text(character.characterClass)
+        Text(character.game)
+      }
     }
   }
 }
